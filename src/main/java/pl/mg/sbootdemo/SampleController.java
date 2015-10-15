688862a0444c6132a0fa57941ff065338370ce01
smieci000 <smieci000@gmail.com>
2015-10-15 22:36:00 +0200

    Initial commit

---
+++ src/main/java/pl/mg/sbootdemo/SampleController.java
@@ -0,0 +1,29 @@
+package pl.mg.sbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by m on 2015-10-15.
+ */
+@Controller
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "sample";
+    }
+
+    public static void main(String[] args){
+        System.out.println("starting boot");
+        SpringApplication.run(SampleController.class, args);
+    }
+
+}
